ff0e6e9209c4a9502db905b018323748df25f9b6
helloyanis <40769776+helloyanis@users.noreply.github.com>
2023-11-25 19:09:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+const LS = browser.storage.local;
+function save(configname) {
+  browser.tabs.executeScript({
+    code:
+      `Array.from(document.querySelectorAll(".room_config_item")).map(node => ({
+        value: node.type === 'checkbox' ? node.checked : node.value, 
+        dataIndex: node.getAttribute('data-index')
+      }))`
+  }).then(saved => {
+    var config = {};
+    saved[0].forEach((item) => {
+      config[item.dataIndex] = item.value;
+    });
+    LS.set({[configname]: JSON.stringify(config)});
+    getAllNames();
+  })
+}
+
+function load(configname){
+  LS.get(configname).then(response => {
+    toLoad = JSON.parse(response[configname]);
+    Object.keys(toLoad).forEach(key => {
+      console.log(toLoad[key]);
+      browser.tabs.executeScript({
+        code:
+          `if(document.querySelector(".room_config_item[data-index='${key}']").type === 'checkbox'){
+            document.querySelector(".room_config_item[data-index='${key}']").checked = ${toLoad[key]};
+            }else{
+              document.querySelector(".room_config_item[data-index='${key}']").value = ${toLoad[key]};
+            }`
+      });
+    });
+    browser.tabs.executeScript({
+      code:
+        `document.querySelector("#room_opts_save").setAttribute('class','ns unsaved');`
+    });
+    document.body.innerHTML = "<h1>Loaded! Don't forget to save.</h1>";
+  });
+}
+function delcfg(configname){
+  console.log(configname);
+  LS.remove(configname).then(() => {
+    getAllNames();
+  });
+}
+function getAllNames(){
+  LS.get().then(ls => {
+    document.querySelector("#savedconfigstable").innerHTML = "";
+        if (Object.keys(ls).length > 0) {
+          console.log("test2");
+          const table = document.querySelector("#savedconfigstable");
+          Object.keys(ls).forEach(name => {
+            const row = document.createElement('tr');
+            const button = document.createElement('button');
+            button.id = name;
+            button.textContent = name;
+            button.addEventListener("click", function(){
+              clickhandler(name);
+            });
+            row.appendChild(button);
+            table.appendChild(row);
+          });
+        }else{
+          document.querySelector("#savedconfigstable").innerHTML = "<tr>Nothing yet.</tr>";
+    }
+    return Object.keys(ls);
+
+
+  });
+  
+}
+function clickhandler(configname){
+  document.querySelector("#savedconfigstable").innerHTML = "<tr><button id='load'>☑️ Load</button> <button id='delete'>🗑️ Delete</button></tr><tr><button id='back'>↩️ Back</button>";
+  console.log("test");
+  document.querySelector("#load").addEventListener("click", function(){
+    browser.tabs.executeScript({
+      code:
+        `document.querySelector("#room_content_room").offsetParent!=null;`
+    }).then(response => {
+      console.log(response);
+    if (!response[0]){
+      document.body.innerHTML = "<h1>❌ Cannot find the config menu on the page!<br>Enter a room first!</h1>";
+    } else {
+      document.body.innerHTML = "<h1>Loading preset!<br>If you see this for a long time, the add-on may have crashed.</h1>";
+      load(configname);
+    }
+  })
+
+  });
+  document.querySelector("#delete").addEventListener("click", function(){
+    delcfg(configname);
+  });
+  document.querySelector("#back").addEventListener("click", function(){
+    getAllNames();
+  });
+}
+//----------------------------------------------
+//Load all names on the main screen
+LS.get().then(response => {
+  console.log(response);
+  
+});
+
+document.querySelector("#save").addEventListener("click", function(){
+  var name = document.querySelector("#configname").value;
+  if (name == "") {
+    name = "My preset";
+  }
+  browser.tabs.executeScript({
+    code:
+      `document.querySelector("#room_content_room").offsetParent!=null;`
+  }).then(response => {
+    if (!response[0]) {
+    document.body.innerHTML = "<h1>❌ Cannot find the config menu on the page!<br>Enter a room first!</h1>";
+  } else {
+    save(name);
+  }
+})
+});
+
+getAllNames();
