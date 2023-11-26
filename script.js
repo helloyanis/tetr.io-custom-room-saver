e163199df7acea1847de6c2f2ddd9c82585d75a2
helloyanis <40769776+helloyanis@users.noreply.github.com>
2023-11-26 13:53:42 +0100

    1.4

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const LS = browser.storage.local;
 function save(configname) {
-  browser.tabs.executeScript({
-    code:
-      `Array.from(document.querySelectorAll(".room_config_item")).map(node => ({
-        value: node.hasAttribute('data-song') ? node.getAttribute('data-song') : (node.type === 'checkbox' ? node.checked : (node.type === 'input' ? node.value : node.innerHTML)),
-        key: node.getAttribute('data-index')
-      }))`
-  }).then(saved => {
-    var config = {};
-    saved[0].forEach((item) => {
-      config[item.key] = item.value;
-    });
-    LS.set({[configname]: JSON.stringify(config)});
-    getAllNames();
-  })
+  LS.get(configname).then(response => {
+    if (Object.keys(response).length > 0 && document.querySelector("#save").innerText!="⚠️ Overwrite?") {
+      return document.querySelector("#save").innerText = "⚠️ Overwrite?";
+    }
+    document.querySelector("#save").innerText = "Saving...";
+    browser.tabs.executeScript({
+      code:
+        `Array.from(document.querySelectorAll(".room_config_item")).map(node => ({
+          value: node.hasAttribute('data-song') ? node.getAttribute('data-song') : (node instanceof HTMLInputElement && node.type === 'checkbox' ? node.checked : (node instanceof HTMLInputElement ? node.value : node.innerHTML)),
+          key: node.getAttribute('data-index')
+        }))`
+    }).then(saved => {
+      var config = {};
+      saved[0].forEach((item) => {
+        config[item.key] = item.value;
+      });
+      LS.set({[configname]: JSON.stringify(config)});
+      document.querySelector("#save").innerText = "✅ Saved!";
+      getAllNames();
+    })
+
+})
 }
 
 function load(configname){
@@ -56,7 +64,6 @@ function getAllNames(){
   LS.get().then(ls => {
     document.querySelector("#savedconfigstable").innerHTML = "";
         if (Object.keys(ls).length > 0) {
-          console.log("test2");
           const table = document.querySelector("#savedconfigstable");
           Object.keys(ls).forEach(name => {
             const row = document.createElement('tr');
@@ -79,8 +86,7 @@ function getAllNames(){
   
 }
 function clickhandler(configname){
-  document.querySelector("#savedconfigstable").innerHTML = "<tr><button id='load'>☑️ Load</button> <button id='delete'>🗑️ Delete</button></tr><tr><button id='back'>↩️ Back</button>";
-  console.log("test");
+  document.querySelector("#savedconfigstable").innerHTML = "<tr><button id='load'>☑️ Load</button> <button id='setcmd'>📜 Get corresponding /set command</button> <button id='delete'>🗑️ Delete</button></tr><tr><button id='back'>↩️ Back</button>";
   document.querySelector("#load").addEventListener("click", function(){
     browser.tabs.executeScript({
       code:
@@ -101,6 +107,48 @@ function clickhandler(configname){
   });
   document.querySelector("#back").addEventListener("click", function(){
     getAllNames();
+  });
+  document.querySelector("#setcmd").addEventListener("click", function(){
+    LS.get(configname).then(response => {
+      toLoad = JSON.parse(response[configname]);
+      var cmdarray = [];
+      var cmd = "/set ";
+      Object.keys(toLoad).forEach(key => {
+        console.log(key + " - " + toLoad[key]);
+        if(toLoad[key] !== "" && key!="options.presets"){
+          var val
+          switch (toLoad[key]) {
+            case "true":
+              val=1;
+              break;
+            case "false":
+              val=0
+              break;
+            default:
+              val=toLoad[key];
+              break;
+          }
+          if ((cmd + key + "=" + val + "; ").length > 512) {
+            cmdarray.push(cmd);
+            cmd = "/set " + key + "=" + val + "; ";
+          }else{
+            cmd += key + "=" + val + "; ";
+          }
+        }
+      });
+      document.body.innerHTML = "<h1>Here are your commands :<br>It's split between multiple commands due to chat length limit<br><table id='commands'></table>Copy them and paste them in the chat!</h1>";
+      cmdarray.push(cmd);
+      cmdarray.forEach(cmd => {
+        const row = document.createElement('tr');
+        const textarea = document.createElement('textarea');
+        textarea.cols = 50;
+        textarea.rows = 3;
+        textarea.value = cmd;
+        textarea.readOnly = true;
+        row.appendChild(textarea);
+        document.querySelector("#commands").appendChild(row);
+      });
+    });
   });
 }
 //----------------------------------------------
